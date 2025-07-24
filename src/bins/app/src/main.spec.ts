0a6ce3cc732b6fcd1ba19b2ba39cd82024e8f36b
Jan Guzman <janfrancisco19@gmail.com>
2025-07-23 23:42:20 -0400

    adding missing prettier config

--- src/bins/app/src/main.spec.ts
+++ src/bins/app/src/main.spec.ts
@@ -1,7 +1,7 @@
-import { expect } from "chai";
+import { expect } from 'chai';
 
-describe("main", () => {
-  it("should pass", async () => {
+describe('main', () => {
+  it('should pass', async () => {
     expect(true).to.eq(true);
   });
 });
