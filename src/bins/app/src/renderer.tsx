998ffc7560d20e3f90bfef8e6db8eabb6971cd6f
Jan Guzman <janfrancisco19@gmail.com>
2025-07-25 15:53:54 -0400

    cleaner react entry point

--- src/bins/app/src/renderer.tsx
+++ src/bins/app/src/renderer.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { App } from './components/App';
 
-function App() {
-  return <h1>Hello from React inside Electron (no Vite)</h1>;
-}
-
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.createRoot(root).render(<App />);
-}
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
